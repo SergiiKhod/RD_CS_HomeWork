4f3044ed9b69b2716f77c2a3e1ce642f59d2cf4f
Sergii Khodatskyi <googsergio@gmail.com>
2023-06-01 21:41:56 +0300

    Write pseudographic "LOVE"

--- HomeWork_1/HomeWork_1/Program.cs
+++ HomeWork_1/HomeWork_1/Program.cs
@@ -1,12 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string nameFirst = "Sergii";
+string? word = string.Empty;
 
-string nameLast = "Khodatskyi";
-
-
-
-Console.WriteLine($"Hello, {nameFirst} {nameLast}!");
+Console.WriteLine($"Homewrok #1");
+Console.WriteLine();
+Console.WriteLine("$$$_____$$$$$$$_$$$$$$$_$$$_______$$$_$$$$$$$$$$");
+Console.WriteLine("$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______");
+Console.WriteLine("$$$____$$$_____$_____$$$_$$$_____$$$__$$$_______");
+Console.WriteLine("$$$_____$$$_________$$$___$$$___$$$___$$$$$$$$__");
+Console.WriteLine("$$$______$$$_______$$$_____$$$_$$$____$$$_______");
+Console.WriteLine("$$$_______$$$_____$$$______$$$_$$$____$$$_______");
+Console.WriteLine("$$$$$$$$$___$$$$$$$_________$$$$$_____$$$$$$$$$$");
 
 Console.ReadLine();
 
