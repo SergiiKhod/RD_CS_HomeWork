3e1a7685c727d47dec1a16aac39affcb629dd679
Sergii Khodatskyi <googsergio@gmail.com>
2023-06-05 21:38:07 +0300

    Homework

--- HomeWork_2/HomeWork_2/Program.cs
+++ HomeWork_2/HomeWork_2/Program.cs
@@ -1,9 +1,13 @@
-﻿Console.WriteLine("Enter an integer:");
+﻿Console.WriteLine(" Enter a number from 10000 to 99999:");
 string? str = Console.ReadLine();
-int number = int.Parse(str);
-int countDigits = str.Length;
-for (int i = countDigits; i > 0; i--)
-{
-    Console.WriteLine((number - (number / (int)Math.Pow(10, i)) * (int)Math.Pow(10, i)) / (int)Math.Pow(10, i - 1));
-}
+int number = Convert.ToInt32(str);
+
+int numberFirst = number / 10000;
+int numberSecond = (number - (number / 10000)*10000)/1000;
+int numberThird = (number - (number / 1000) * 1000) / 100;
+int numberFourth = (number - (number / 100) * 100) / 10;
+int numberFifth = (number - (number / 10) * 10) / 1;
+
+Console.WriteLine($"{numberFirst} {numberSecond} {numberThird} {numberFourth} {numberFifth}");
+
 Console.ReadLine();
