d54a1e0053f08abfb42c9ba843545e806061c5a3
Sergii Khodatskyi <googsergio@gmail.com>
2023-06-05 21:14:39 +0300

    WriteDigits

---
+++ HomeWork_2/HomeWork_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter an integer:");
+string? str = Console.ReadLine();
+int number = int.Parse(str);
+int countDigits = str.Length;
+for (int i = countDigits; i > 0; i--)
+{
+    Console.WriteLine((number - (number / (int)Math.Pow(10, i)) * (int)Math.Pow(10, i)) / (int)Math.Pow(10, i - 1));
+}
+Console.ReadLine();
