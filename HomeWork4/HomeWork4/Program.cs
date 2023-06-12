fbd5f3985c162db94cb4a9c58f70a489a8af62de
Sergii Khodatskyi <googsergio@gmail.com>
2023-06-12 21:05:38 +0300

    Summary numbers

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,18 @@
+﻿const int N = 5;
+
+int[] numbers = new int[N];
+for (int i = 0;i < N; i++)
+{
+    Console.Write($"Enter a number № {i + 1}: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int numbersSummary = 0;
+foreach(int number in numbers)
+{
+    numbersSummary += number;
+}    
+
+Console.WriteLine($"Summary: {numbersSummary}");
+
+
